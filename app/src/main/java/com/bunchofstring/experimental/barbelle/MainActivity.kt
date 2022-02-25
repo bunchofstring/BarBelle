f52ee5b3c9ff4da9395ee9e5aef398689d019d17
Dave Mitchell <dave@bunchofstring.com>
2022-02-25 12:07:18 -0500

    Initial commit

---
+++ app/src/main/java/com/bunchofstring/experimental/barbelle/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.bunchofstring.experimental.barbelle
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.view.MotionEvent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.NestedScrollView
+
+/**
+ * 1. When the content starts scrolling, then highlight the scrollbar thumb
+ * 2. When the content stops scrolling, then un-highlight the scrollbar thumb
+ * 3. Note: Ideally the solution can easily be applied for all scrollable views (1) within one app (2) within all apps running on a particular android user (3) within any installed app
+ */
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        manageThumbState(findViewById(R.id.exampleScrollView))
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    private fun manageThumbState(sv: NestedScrollView){
+        sv.setOnScrollChangeListener(NestedScrollView.OnScrollChangeListener { v, scrollX, scrollY, oldScrollX, oldScrollY ->
+            v.isPressed = true
+        })
+        sv.setOnTouchListener { p0, p1 ->
+            if (p1?.action == MotionEvent.ACTION_UP) {
+                p0?.postDelayed({
+                    p0.isPressed = false
+                }, 1000) //TODO: Eliminate this ugly delay. Problem is that the scrolling animation may take Ideally, isPressed is set after the scrolling animation ends.
+            }
+            false
+        }
+    }
+}
